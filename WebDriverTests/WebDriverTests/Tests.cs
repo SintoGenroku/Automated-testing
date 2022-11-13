7d4c324424f6e449b6f428a85e75a2987c2da3dc
SintoGenroku <borozda.a.s@gmail.com>
2022-11-13 22:46:32 +0300

    add webdriver lab

---
+++ WebDriverTests/WebDriverTests/Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverTests.Helpers;
+using WebDriverTests.Pages;
+
+namespace WebDriverTests
+{
+    [TestFixture]
+    class Test
+    {
+        WebDriver driver;
+
+        [SetUp]
+        public void Startup()
+        {
+            driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(200);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(200);
+        }
+
+        [Test]
+        public void IsComparingInaccessibilityMessageShownCorrectly_ReturnsTrue()
+        {
+            var samsungPage = new SamsungPage(driver);
+            samsungPage.OpenPage();
+            samsungPage.OpenLoginWindow();
+            samsungPage.InputLogin();
+            samsungPage.InputPasswordAndConfirm();
+            samsungPage.GoToTablets();
+
+            Assert.IsTrue(samsungPage.IsInaccessibilityMessageShown);
+        }
+
+    }
+}
